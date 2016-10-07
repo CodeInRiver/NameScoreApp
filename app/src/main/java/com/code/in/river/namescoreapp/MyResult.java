c8abfb2dbda355f13aabdaf9e184c392af6d0f01
kawauchi <yasunari.kawauchi@gmail.com>
2016-10-08 08:44:15 +0900

    develop complete

--- app/src/main/java/com/code/in/river/namescoreapp/MyResult.java
+++ app/src/main/java/com/code/in/river/namescoreapp/MyResult.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MyResult extends AppCompatActivity {
 
     @Override
@@ -17,5 +19,12 @@ public class MyResult extends AppCompatActivity {
         String myName = intent.getStringExtra(MyForm.EXTRA_NAME);
         TextView nameLabel = (TextView) findViewById(R.id.nameLabel);
         nameLabel.setText(myName + "の点数は...");
+
+        Random randomGenerator = new Random();
+        int score = randomGenerator.nextInt(101); // 0-100
+
+        TextView scoreLabel = (TextView) findViewById(R.id.scoreLabel);
+        scoreLabel.setText(String.valueOf(score) + "点!!");
+
     }
 }
